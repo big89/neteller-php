c38ae0bc1c0c210d6d8fee4d603c8e7063d7f5b1
Bhagwan Sahane <big89@users.noreply.github.com>
2017-05-10 09:45:59 +0530

    Create WebhookHandler.php

---
+++ api/WebhookHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Class WebhookHandler
+ * @package grandmasterx\neteller\api
+ */
+class WebhookHandler extends NetellerApi
+{
+
+    /**
+     *
+     */
+    public function handleRequest() {
+        if (isset($_POST)) {
+            $webhookData = file_get_contents("php://input");
+            $webhookData = json_decode($webhookData);
+
+            if (function_exists($webhookData->eventType)) {
+                call_user_func($webhookData->eventType, $webhookData);
+            }
+
+            header('X-PHP-Response-Code: 200', true, 200);
+        }
+
+    }
+}
